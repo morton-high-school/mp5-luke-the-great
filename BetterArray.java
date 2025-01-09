41d61abd2837e1d82074d2abd9d5d60d7727584d
luke-the-great <luke.baker@mcusd709.org>
2025-01-09 08:46:13 -0600

    yes

---
+++ BetterArray.java
@@ -0,0 +1,72 @@
+public class BetterArray{
+    
+    private static int size;
+    public int[] array;
+    
+
+    public BetterArray(){
+        size = 0;
+        int[] array = new int[0];
+    }
+
+    public BetterArray(int size){
+        this.size = size;
+        int[] array = new int[size];
+    }
+
+    public static int size(){
+        return size;
+    }
+// Returns the number of elements in the list
+
+    public boolean add(int item){
+        array[size] = item;
+        size++;
+        if(size >= array.length){
+            int[] newArray = new int[array.length*2];
+        }
+        return true;
+    }
+// appends item to the end of the list; returns true
+
+    public void add(int index, int item){
+        int var = array[index];
+        array[index] = item;
+        for(int i=index+1; i<array.length; i++){
+            int skib = var;
+            var = array[i];
+            array[i] = skib;
+        }
+        size++;
+        if(size >= array.length){
+            int[] newArray = new int[array.length*2];
+        }
+    }
+// Inserts item at position index, moving elements at position index and higher to
+// the right and adds 1 to size
+
+    public int get(int index){
+        int item = array[index];
+        return item;
+    }
+// Returns the element at position index in the list
+
+    public static int set(int index, int item){
+        int temp = array[index];
+        array[index] = item;
+        return temp;
+    }
+// Replaces the element at position index with item; returns the element formerly
+// at position index
+
+    public int remove(int index){
+        int temp = index;
+        this.index = 0;
+        size--;
+        return temp;
+    }
+// Removes element from position index, moving elements at position index+1 and
+// higher to the left and subtracts 1 from size; returns the element formerly at
+// position index
+
+}
